808e619b2ab9c3a05cea0fad33938b420e32d0b6
matt <mbbyrnes@gmail.com>
2024-08-14 11:00:19 -0700

    tweaks

---
+++ laravel_init.sh
@@ -0,0 +1,4 @@
+# commands to set laravel configs, folders and files when running for the first time
+If app:key still empty on .env run php artisan key:generate on your terminal after going into the php container on docker
+cp .env.example .env
+
